3da880215cd7c09daae4b5dc87ab0f144e068681
user_1755501034113 <email_1755501034113>
2025-12-29 17:32:49 +0800

    feat: update build usockets

--- external/uSockets_external.cmake
+++ external/uSockets_external.cmake
@@ -1,29 +1,16 @@
 include(ExternalProject)
 
-
 ExternalProject_Add(
-    uSockets_external
-
-
-    URL               ${3RDPARTY_ZIP_PATH}/uSockets-0.8.8.zip
-    SOURCE_DIR        ${CMAKE_BINARY_DIR}/uSockets-src
-    BINARY_DIR        ${CMAKE_BINARY_DIR}/uSockets-build
-    INSTALL_DIR       ${PERICHECK_INSTALL_DIR}
-    
-    # CMAKE_ARGS
-    #     -DCMAKE_INSTALL_PREFIX=${PERICHECK_INSTALL_DIR}
-    #     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    #     -DLIBUV_BUILD_TESTS=OFF 
-        
-    # BUILD_COMMAND     ${CMAKE_COMMAND} --build <BINARY_DIR> --config ${CMAKE_BUILD_TYPE}
-    
-    # UPDATE_COMMAND    ""
-    
-    # LOG_DOWNLOAD      ON
-    # LOG_CONFIGURE     ON
-    # LOG_BUILD         ON
-    # LOG_INSTALL       ON
+    uSockets_unpack
+    URL ${3RDPARTY_ZIP_PATH}/uSockets-0.8.8.zip
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/uSockets-src
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+    LOG_DOWNLOAD ON
 )
+
 
 
 set(USOCKETS_INCLUDE_DIR ${PERICHECK_INSTALL_DIR}/include)
